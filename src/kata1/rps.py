f586a3b31447a7689a308c4f772ddcaffb04808d
Pablo Casas <casdelprog@gmail.com>
2020-05-16 16:45:57 +0200

    Kata 1

--- src/kata1/rps.py
+++ src/kata1/rps.py
@@ -1,13 +1,39 @@
 from random import randint
+import random
 
 options = ["Piedra", "Papel", "Tijeras"]
+
 
 # El resultado de salida son las siguientes String
 #'Empate!'
 #'Ganaste!'
 #'Perdiste!'
 def quienGana(player, ai):
-    return ""
+    player = player.lower()
+    ai = ai.lower()
+    resultado = ""
+    if player == options[0]:
+        if ai == options[0]:
+            resultado = "Empate!"
+        elif ai == options[1]:
+            resultado = "Perdiste!"
+        elif ai == options[2]:
+            resultado = "Ganaste!"
+    elif player == options[1]:
+        if ai == options[0]:
+            resultado = "Ganaste!"
+        elif ai == options[1]:
+            resultado = "Empate!"
+        elif ai == options[2]:
+            resultado = "Perdiste!"
+    elif player == options[2]:
+        if ai == options[0]:
+            resultado = "Perdiste!"
+        elif ai == options[1]:
+            resultado = "Ganaste!"
+        elif ai == options[2]:
+            resultado = "Empate!"
+    return resultado
 
 # Entry Point
 def Game():
@@ -16,7 +42,8 @@ def Game():
     
     #
     #
-    
+    player = input("Escribe: Piedra, Papel o Tijeras")
+    ai = options[random.choice(range(3))]
     winner = quienGana(player, ai)
 
     print(winner)
