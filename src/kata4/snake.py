3f82783627a7757e8c0a66d3328e3bac0bd9d4da
Pablo Casas <casdelprog@gmail.com>
2020-05-16 20:06:35 +0200

    Descomentados comandos para jugar ( no test )

--- src/kata4/snake.py
+++ src/kata4/snake.py
@@ -107,9 +107,9 @@ class Game():
 def main():
     # Descomentar para lanzar el juego en local
     # Comentar para validar con el oráculo
-    #pygame.init()
-    #play_surface = pygame.display.set_mode((500, 500))
-    #fps = pygame.time.Clock()
+    pygame.init()
+    play_surface = pygame.display.set_mode((500, 500))
+    fps = pygame.time.Clock()
 
     snake = Snake()
     game = Game()
@@ -141,5 +141,5 @@ def main():
 # Comienza la aventura!!!!
 # Descomentar para lanzar el juego en local
 # Comentar para validar con el oráculo
-#main()
-#pygame.quit()
+main()
+pygame.quit()
